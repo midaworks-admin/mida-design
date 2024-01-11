93198094e347c7ed677b0975e6d834abaff5d93e
Suin Kim <contact@suinkim.me>
2024-01-11 12:30:05 +0900

    삭제

--- view/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as Color} from './color/Color';
-export {default as Component} from './component/Component';
-export {default as Icon} from './icon/Icon';
-export {default as Vibration} from './vibration/Vibration';
